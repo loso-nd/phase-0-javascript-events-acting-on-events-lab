e14166f1f7ce87d5958041960c5569b5e954877b
Nahum <losond8@gmail.com>
2021-02-13 18:41:27 -0800

    Moving Things with JS by Acting on Events. How to move an element in response to a browser event. Learn to update an element's position on the page conditionally.

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 // Your code here
+
+// get an element by id and store it in a variable called dodger   
+const dodger = document.getElementById('dodger');
+
+dodger.style.backgroundColor = "#FF69B4";
+
+dodger.style.bottom = "0px"
+dodger.style.left = "180px"
+
+let moveDodgerLeft = () => {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if (left > 0){
+        dodger.style.left = `${left - 10}px`;
+    }
+}
+
+document.addEventListener('keydown', function(e){
+    if (e.key === "ArrowLeft") {
+        moveDodgerLeft();
+    }
+})
+
+let moveDodgerRight = () => {
+    const rightNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(rightNumbers, 10);
+
+    if (left > 0){
+        dodger.style.left = `${left + 10}px`;
+    }
+}
+
+document.addEventListener('keydown', function(e){
+    if (e.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+})
+
+// document.addEventListener("keydown", function(event) {
+//     if (event.key === "ArrowLeft") {
+//       const leftNumbers = dodger.style.left.replace("px", "");
+//       const left = parseInt(leftNumbers, 10);
+  
+//       dodger.style.left = `${left - 10}px`;
+//     }
+//   }); 
